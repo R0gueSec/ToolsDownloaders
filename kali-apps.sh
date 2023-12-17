b1e544a72994a0756e3c845ae1b5a1b6019cfa0a
R0gueSec <143247134+R0gueSec@users.noreply.github.com>
2023-12-16 16:20:06 -0800

    Add files via upload
    Installs additional apps

---
+++ kali-apps.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+sudo apt update
+sudo apt install flameshot
+sudo apt-get install libreoffice
+# Installing Brave Browser
+sudo apt install curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
+# Installing ScreenSaver
+sudo apt -y install kali-screensaver
+# Installing Obsidian
+wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.4.16/obsidian_1.4.16_amd64.deb
+sudo dpkg -i obsidian_1.4.16_amd64.deb
+# Cleaning Up
+rm obsidian_1.4.16_amd64.deb
+# Installing NetExec (nxc)
+wget https://github.com/Pennyw0rth/NetExec/releases/download/v1.1.0/nxc
+chmod +x nxc
+mv nxc /home/kali/.local/bin
+sudo apt install remmina
+sudo apt install gedit
