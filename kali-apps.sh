f0b5674bf9d984ed44c945a744cc6eb1e2e56421
R0gueSec <143247134+R0gueSec@users.noreply.github.com>
2024-12-12 19:35:47 -0800

    Update kali-apps.sh
    Updated obsidian version
    commented out nxc since it comes default with kali

--- kali-apps.sh
+++ kali-apps.sh
@@ -21,14 +21,14 @@ sudo apt install brave-browser
 # Installing ScreenSaver
 sudo apt -y install kali-screensaver
 # Installing Obsidian
-wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.4.16/obsidian_1.4.16_amd64.deb
+wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.7.7/obsidian_1.7.7_amd64.deb
 sudo dpkg -i obsidian_1.4.16_amd64.deb
 # Cleaning Up
 rm obsidian_1.4.16_amd64.deb
 # Installing NetExec (nxc)
-wget https://github.com/Pennyw0rth/NetExec/releases/download/v1.1.0/nxc
-chmod +x nxc
-sudo mv nxc /usr/local/bin
+#wget https://github.com/Pennyw0rth/NetExec/releases/download/v1.1.0/nxc
+#chmod +x nxc
+#sudo mv nxc /usr/local/bin
 sudo apt install remmina
 sudo apt install gedit
 sudo apt update && sudo apt install -y bloodhound
