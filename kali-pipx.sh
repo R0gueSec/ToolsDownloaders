e2cfdb02a223bd565450d521a4ac389f9ac067e8
R0gueSec <143247134+R0gueSec@users.noreply.github.com>
2025-03-18 23:45:31 -0700

    Update kali-pipx.sh
    Made the following changes to lines 18-20:
    
    sudo apt install seclists curl dnsrecon enum4linux feroxbuster gobuster impacket-scripts nbtscan nikto nmap onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb
    #wkhtmltopdf is no longer in kali repository. It must be installed manually
    wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.buster_amd64.deb

--- kali-pipx.sh
+++ kali-pipx.sh
@@ -15,7 +15,11 @@ sudo apt install python3-venv
 python3 -m pip install --user pipx
 python3 -m pipx ensurepath
 #Installing Autorecon tools
-sudo apt install seclists curl dnsrecon enum4linux feroxbuster gobuster impacket-scripts nbtscan nikto nmap onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf
+sudo apt install seclists curl dnsrecon enum4linux feroxbuster gobuster impacket-scripts nbtscan nikto nmap onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb
+#wkhtmltopdf is no longer in kali repository. It must be installed manually
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.buster_amd64.deb
+sudo dpkg -i wkhtmltox_0.12.6-1.buster_amd64.deb
+rm wkhtmltox_0.12.6-1.buster_amd64.deb
 #installing autorecon, impacket, and uploadserver
 pipx install git+https://github.com/Tib3rius/AutoRecon.git
 python3 -m pipx install impacket
